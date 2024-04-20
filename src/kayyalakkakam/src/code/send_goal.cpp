f483cdde35d947272fb3eb95881b8cd212386460
CoCoKayya18 <collins1999.ck@gmail.com>
2024-04-20 17:29:06 +0200

    modified:   src/kayyalakkakam/src/code/send_goal.cpp 	modified:   src/kayyalakkakam/src/launch/run_turtlebot3.launch''

--- src/kayyalakkakam/src/code/send_goal.cpp
+++ src/kayyalakkakam/src/code/send_goal.cpp
@@ -7,94 +7,31 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-// Function to move the robot to the specified goal
-void moveToGoal(MoveBaseClient& client, double x_goal, double y_goal, double yaw_goal) {
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "simple_navigation_goals");
+    
+    //tell the action client that we want to spin a thread by default
+    MoveBaseClient ac("move_base", true);
+
+    //wait for the action server to come up
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ros::ROS_LOGINFO("Waiting for the move_base action server to come up");
+    }
     move_base_msgs::MoveBaseGoal goal;
-
-    // Set up the frame parameters
-    goal.target_pose.header.frame_id = "map";  // Change to your frame ID
+    //we'll send a goal to the robot to move 1 meter forward
+    goal.target_pose.header.frame_id = "base_link";
     goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = 1.0;
+    goal.target_pose.pose.orientation.w = 1.0;
 
-    // Define position
-    goal.target_pose.pose.position.x = x_goal;
-    goal.target_pose.pose.position.y = y_goal;
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+    ac.waitForResult();
 
-    // Convert yaw to quaternion using tf library
-    tf::Quaternion q = tf::createQuaternionFromYaw(yaw_goal);
-    geometry_msgs::Quaternion q_msg;
-    tf::quaternionTFToMsg(q, q_msg);
-    goal.target_pose.pose.orientation = q_msg;
-
-    // Send the goal to the action server
-    client.sendGoal(goal);
-
-    // Wait for the action server to complete the goal
-    if (client.waitForResult()) {
-        if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-            ROS_INFO("Successfully reached the waypoint.");
-        else
-            ROS_INFO("Failed to reach the waypoint.");
-    } else {
-        ROS_ERROR("Action server not available!");
-        ros::shutdown();
-    }
-}
-
-// Function to initialize the action client
-MoveBaseClient initializeActionClient() {
-    MoveBaseClient client("move_base", true);
-    client.waitForServer();
-    return client;
-}
-
-// Function to stop the robot
-void stopRobot() {
-    ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-    ros::Duration(1.0).sleep(); // Sleep to give the publisher time to connect
-
-    geometry_msgs::Twist stop_msg; // Default constructor initializes to zero
-    pub.publish(stop_msg);
-    ROS_INFO("Robot stopped.");
-}
-
-// Function to stop ROS master and all nodes
-void stopRosMaster() {
-    ROS_INFO("Stopping ROS master and all nodes.");
-    system("rosnode kill -a");
-    system("killall -9 rosmaster");
-    system("killall -9 roscore");
-}
-
-// Function to execute movement in a square pattern
-void moveSquare(MoveBaseClient& client) {
-    std::vector<std::tuple<double, double, double>> waypoints = {
-        std::make_tuple(-1.5, 1.5, M_PI/2),
-        std::make_tuple(-1.5, -1.5, M_PI),
-        std::make_tuple(1.5, -1.5, -M_PI/2),
-        std::make_tuple(1.5, 1.5, 0)
-    };
-
-    for (auto& waypoint : waypoints) {
-        double x_goal, y_goal, yaw_goal;
-        std::tie(x_goal, y_goal, yaw_goal) = waypoint;
-        ROS_INFO("Moving to waypoint: x = %f, y = %f meters", x_goal, y_goal);
-        moveToGoal(client, x_goal, y_goal, yaw_goal);
-        ros::Duration(1.0).sleep(); // Short pause at each waypoint
-    }
-}
-
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "move_base_square_cpp");
-    MoveBaseClient client = initializeActionClient();
-
-    try {
-        moveSquare(client);
-        stopRobot(); // Ensure the robot is stopped after completing the square
-        stopRosMaster(); // Shutdown ROS master
-    } catch (const ros::Exception& e) {
-        ROS_ERROR("Navigation sequence interrupted: %s", e.what());
-    }
-
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Hooray, the base moved 1 meter forward");
+    else
+        ROS_INFO("The base failed to move forward 1 meter for some reason");
     return 0;
 }
